490e40c4cf35fb51853e6526e7a8591976769ea5
Kedar <kedar@jymob.com>
2015-04-19 12:25:12 -0700

    another intermed version

--- games/trash/trash.js
+++ games/trash/trash.js
@@ -1,20 +1,20 @@
 // suit: one of 'spades', 'hearts', 'diamonds', 'clubs'
-var is_valid_suit = function is_valid_suit(suit) {
+var isValidSuit = function isValidSuit(suit) {
   return suit === 'spades' || suit === 'hearts' || suit === 'clubs' || suit === 'diamonds';
 };
 // rank: [1, 13]: 1 for ace, 11, 12, 13 for jack, queen and king respectively
-var is_valid_rank = function is_valid_rank(rank) {
+var isValidRank = function isValidRank(rank) {
   return typeof rank === 'number' && rank >= 1 && rank <= 13;
 };
 var card = function card(suit, rank) {
   var s = suit, r = rank;
-  if (! is_valid_suit(s)) {
+  if (! isValidSuit(s)) {
     throw {
       name: 'TypeError',
       message: 'suit: ' + s + ' is invalid'
     };
   }
-  if (! is_valid_rank(r)) {
+  if (! isValidRank(r)) {
     throw {
       name: 'TypeError',
       message: 'rank: ' + r + ' is invalid'
@@ -30,6 +30,33 @@ var card = function card(suit, rank) {
   };
 }
 var player = function player(name) {
-  return {};
-}
-console.log(card('spades', 2).rank());
+  return {
+    play: function play() {
+    },
+    hasWon: function hasWon() {
+    }
+  };
+};
+// returns a game that has a pickupPile, a discardPile and two players
+// the caller can then start the game which continues till a player has won 
+// players take turn
+// the game starts in a non-won state and a player can win after her/his turn
+var game = function game(name1, name2) {
+  var player1, player2, current_player;
+  return {
+    begin: function begin() {
+      //deal
+      player1 = player(name1);
+      player2 = player(name2);
+      current_player = player1;
+      while (true) {
+        current_player.play();
+        if (current_player.hasWon()) {
+          return current_player;
+        }
+        current_player = current_player === player1 ? player2 : player1;
+      }
+    }
+  };
+};
+game().begin();
